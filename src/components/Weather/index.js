7208b1234d17ff7f51584d8fe54aa9670dc8c44b
Jack <jack.x.chen@outlook.com>
2024-01-23 16:09:27 +1000

    Updated Weather component

--- src/components/Weather/index.js
+++ src/components/Weather/index.js
@@ -1,16 +1,27 @@
 import "./styles.css"
 
 const Weather = ({ weather }) => {
+    const InfoDelegate = ({name, value, unit}) => {
+        return <div className="d-flex flex-column align-items-center px-3 py-2">
+            <div>{name}</div>
+            <div className="fw-bold">{value}{unit}</div>
+        </div>
+    }
+
     return (
         weather  
         ? <div className="card | d-flex flex-column align-items-center gap-2 px-3 py-4">
-            <div className="display-4 fw-bold">{weather.current.temp_c}</div>
+            <p className="card__location">{weather.location.name}, {weather.location.country}</p>
             <div className="d-flex flex-column align-items-center">
-                <p className="display-6">{weather.location.name}</p>
-                <p>{weather.location.country}</p>
+                <img className="card__img" src={weather.current.condition.icon}/>
+                <p className="card__condition">{weather.current.condition.text}</p>
             </div>
-            <p className="card__condition">{weather.current.condition.text}</p>
-           <img className="card__img" src={weather.current.condition.icon}/>
+            <div className="card__temp fw-bold">{weather.current.temp_c}&#8451;</div>
+            <div className="d-flex justify-content-center gap-3"> 
+                <InfoDelegate name="Humidity" value={weather.current.humidity} unit="%" />
+                <InfoDelegate name="Wind" value={weather.current.wind_kph} unit="km/hr" />
+                <InfoDelegate name="Precipation" value={weather.current.precip_in} unit="%" />
+            </div>
         </div> 
         : <div>Weather is not available for this location.</div>
     )
